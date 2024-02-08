16a439d6945ab0a8d2e522e8a0ce9efb47da28fa
Sawyer Crawley <crawleysawyer@gmail.com>
2024-02-07 19:42:40 -0700

    changed login.js

--- screens/Login.js
+++ screens/Login.js
@@ -56,7 +56,7 @@ const Login = ({loggedInState, loggedInStates,setLoggedInState})=>{
             <TouchableOpacity
                style={styles.sendButton}
               onPress={async ()=>{
-                console.log(phoneNumber+' Button was pressed')
+                console.log(phoneNumber+' Hello, Sawyer.')
     
                 const sendTextResponse=await fetch(
                   'https://dev.stedi.me/twofactorlogin/'+phoneNumber,
